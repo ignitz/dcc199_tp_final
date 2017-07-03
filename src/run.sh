06237d7bf2594ea820e0c5a6caf02b02acbdb0f9
ignitz <ignitzhjfk@gmail.com>
2017-07-02 21:27:07 -0300

    Little more tests

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,7 @@
 echo 'Testes-----' > tests/out/output.txt
 
+printf '\nn = 10\n' >> tests/out/output.txt
+
 START=$(date +%s.%N)
 ./push_relabel < tests/input10A.txt >> tests/out/output.txt
 END=$(date +%s.%N)
@@ -11,6 +13,8 @@ START=$(date +%s.%N)
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 echo $DIFF >> tests/out/output.txt
+
+printf '\nn = 100\n' >> tests/out/output.txt
 
 START=$(date +%s.%N)
 ./push_relabel < tests/input100A.txt >> tests/out/output.txt
@@ -36,6 +40,8 @@ END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 echo $DIFF >> tests/out/output.txt
 
+printf '\nn = 200\n' >> tests/out/output.txt
+
 START=$(date +%s.%N)
 ./push_relabel < tests/input200A.txt >> tests/out/output.txt
 END=$(date +%s.%N)
@@ -60,6 +66,8 @@ END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 echo $DIFF >> tests/out/output.txt
 
+printf '\nn = 1000\n' >> tests/out/output.txt
+
 START=$(date +%s.%N)
 ./push_relabel < tests/input1000A.txt >> tests/out/output.txt
 END=$(date +%s.%N)
@@ -80,6 +88,22 @@ echo $DIFF >> tests/out/output.txt
 
 START=$(date +%s.%N)
 ./push_relabel < tests/input1000D.txt >> tests/out/output.txt
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo $DIFF >> tests/out/output.txt
+
+printf '\nn = 2000\n' >> tests/out/output.txt
+
+START=$(date +%s.%N)
+./push_relabel < tests/input2000A.txt >> tests/out/output.txt
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo $DIFF >> tests/out/output.txt
+
+printf '\nn = 3000\n' >> tests/out/output.txt
+
+START=$(date +%s.%N)
+./push_relabel < tests/input3000A.txt >> tests/out/output.txt
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 echo $DIFF >> tests/out/output.txt
